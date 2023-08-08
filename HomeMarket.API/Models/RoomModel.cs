339722d5626d2a51a73c5cd83a11c31eb7927bb7
jamol <jamol@LAPTOP-5TNOU2IH>
2023-08-08 09:21:57 +0500

    jamolmeyliyev/add-model-room

---
+++ HomeMarket.API/Models/RoomModel.cs
@@ -0,0 +1,29 @@
+﻿using HomeMarket.API.Entities;
+using System.Runtime.CompilerServices;
+
+namespace HomeMarket.API.Models;
+
+public class RoomModel
+{
+    public Guid Id { get; set; }
+    public Guid HomeId { get; set; }
+    public ERoomType Type { get; set; }
+    public EComfort ComfortType { get; set; }
+    public float Length { get; set; }
+    public float Width { get; set; }
+}
+
+public static class ExtensionRoomModel
+{
+    public static RoomModel ToModel(this Room room)
+    {
+        return new RoomModel
+        {
+            Id = room.Id,
+            HomeId = room.HomeId,
+            Type = room.Type,
+            Length = room.Length,
+            Width = room.Width,
+        };
+    }
+}
