a3fddbc011f513c110891ed62e5c4e6ae07e5cf3
jamol <jamol@LAPTOP-5TNOU2IH>
2023-08-08 09:22:42 +0500

    jamolmeyliyev/add-model-room-create

---
+++ HomeMarket.API/Models/CreateModels/CreateRoomModel.cs
@@ -0,0 +1,12 @@
+﻿using HomeMarket.API.Entities;
+
+namespace HomeMarket.API.Models.CreateModels
+{
+    public class CreateRoomModel
+    {
+        public ERoomType Type { get; set; }
+        public EComfort ComfortType { get; set; }
+        public float Length { get; set; }
+        public float Width { get; set; }
+    }
+}
