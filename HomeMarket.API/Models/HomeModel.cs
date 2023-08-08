6cd9fc3c6bf267c838e6d5aac194fd36a60809f8
jamol <jamol@LAPTOP-5TNOU2IH>
2023-08-08 09:19:53 +0500

    jamolmeyliyev/add-model-home1

---
+++ HomeMarket.API/Models/HomeModel.cs
@@ -0,0 +1,38 @@
+﻿using HomeMarket.API.Controllers;
+using HomeMarket.API.Entities;
+
+namespace HomeMarket.API.Models;
+
+public class HomeModel
+{
+    public Guid Id { get; set; }
+    public required List<Room> Rooms { get; set; }
+    public long Price { get; set; }
+    public float Area { get; set; }
+    public EComfort Type { get; set; }
+    public float EmptyArea { get; set; }
+}
+
+public static class ExtensionHomeModel
+{
+    public static HomeModel ToModel(this Home home)
+    {
+        var rooms = new List<Room>();
+        if (home.Rooms != null)
+        {
+            foreach (var room in home.Rooms)
+            {
+                rooms.Add(room);
+            }
+        }
+        return new HomeModel
+        {
+            Id= home.Id,
+            Rooms = rooms,
+            Price = home.Price,
+            Area = home.Area,
+            Type = home.Type
+        };
+    }
+}
+       
