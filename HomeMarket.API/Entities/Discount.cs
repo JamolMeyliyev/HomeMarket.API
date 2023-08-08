a971a6633d373455f1f2ecb0978a54544f4ac6e3
jamol <jamol@LAPTOP-5TNOU2IH>
2023-08-08 09:00:00 +0500

    jamolmeyliyev/add-model-discount1

---
+++ HomeMarket.API/Entities/Discount.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeMarket.API.Entities;
+
+public class Discount
+{
+    public Guid Id { get; set; }
+    public Guid? HomeId { get; set; }
+    public Home? Home { get; set; }
+    public int Percentage { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
