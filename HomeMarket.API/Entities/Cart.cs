ae731f75ff691b8308b2484426f927b40066de6b
jamol <jamol@LAPTOP-5TNOU2IH>
2023-08-08 08:52:14 +0500

    jamolmeyliyev/add-model-cart

---
+++ HomeMarket.API/Entities/Cart.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeMarket.API.Entities;
+
+public class Cart
+{
+    public Guid Id { get; set; }
+    public Guid HomeId { get; set; }
+    public Home? Home { get; set; }
+    public Guid UserId { get; set; }
+    public User? User { get; set; }
+    public long TotalPrice { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime EndDate { get; set; }
+
+}
